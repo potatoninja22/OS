a98ac30d8275d75855bc12b28cd84046fe863f99
Prasun Nayak <prasun.nayak4@gmail.com>
2023-08-31 21:46:09 +0530

    working qsort

--- ass2/main.c
+++ ass2/main.c
@@ -11,6 +11,7 @@ struct Process {
     double completionTime;
 };
 
+//return type
 typedef struct{
     double startTime;
     double endTime;
@@ -32,6 +33,8 @@ int startTimeComparator(const void *a, const void *b) {
     }
     return 0;
 }
+
+
 double durationComparator(const void *a, const void *b) {
     struct Process *p1 = *(struct Process **)a;
     struct Process *p2 = *(struct Process **)b;
@@ -41,38 +44,41 @@ double durationComparator(const void *a, const void *b) {
 //........................................................................................................................................
 
 typedef struct {
-    char output[1000000];
+    struct PairPair* schedule;
     double avgTurnaroundTime;
     double avgResponseTime;
-} FCFSOutput;
+} Output;
 
-FCFSOutput fcfs(struct Process **vp, int n) {
+Output fcfs(struct Process *vp, int n) {
 
-    FCFSOutput output;
-    output.output[0] = '\0';
-    qsort(vp, n, sizeof(struct Process *), startTimeComparator);
-    double totalTurnaroundTime = 0.0;
-    double totalResponseTime = 0.0;
-    double currTime = 0.0;
-    
-    for (int i = 0; i < n; i++) {
-        currTime = (vp[i]->startTime > currTime) ? vp[i]->startTime : currTime;
-        double endTime = currTime + vp[i]->completionTime;
-        
-        char line[10000];
-        sprintf(line, "%s %.6lf %.6lf ", vp[i]->pid, currTime, endTime);
-        strcat(output.output, line);
-        
-        totalTurnaroundTime += endTime - vp[i]->startTime;
-        totalResponseTime += currTime - vp[i]->startTime;
-        
-        currTime = endTime;
+    Output out;
+
+    qsort(vp, n, sizeof(struct Process), startTimeComparator);
+
+    for(int i=0;i<n;i++){
+        printf("pid: %s\n", vp[i].pid);
     }
+    // double totalTurnaroundTime = 0.0;
+    // double totalResponseTime = 0.0;
+    // double currTime = 0.0;
     
-    output.avgTurnaroundTime = totalTurnaroundTime / n;
-    output.avgResponseTime = totalResponseTime / n;
+    // for (int i = 0; i < n; i++) {
+    //     currTime = (vp[i].startTime > currTime) ? vp[i].startTime : currTime;
+    //     double endTime = currTime + vp[i].completionTime;
+        
+    //     char line[10000];
+    //     sprintf(line, "%s %.6lf %.6lf ", vp[i].pid, currTime, endTime);
+        
+    //     totalTurnaroundTime += endTime - vp[i].startTime;
+    //     totalResponseTime += currTime - vp[i].startTime;
+        
+    //     currTime = endTime;
+    // }
     
-    return output;
+    // out.avgTurnaroundTime = totalTurnaroundTime / n;
+    // out.avgResponseTime = totalResponseTime / n;
+    
+    return out;
 }
 
 //.......................................................................................................................................
@@ -118,28 +124,18 @@ int main(int argc, char *argv[]) {
         printf("%s %0.6f %0.f\n",processes[i].pid,processes[i].startTime,processes[i].completionTime);
     }
 
-    struct Process **processesArray = malloc(numEntries * sizeof(struct Process *));
-    for (int i = 0; i < numEntries; i++) {
-        processesArray[i] = &processes[i];
-    }
-
-    FCFSOutput fcfsStats = fcfs(processesArray, numEntries);
+    Output outfcfs = fcfs(processes, numEntries);
 
     FILE *outputFile = fopen(outputFileName, "w");
     if (!outputFile) {
         printf("Error opening output file.\n");
         free(processes);
-        free(processesArray);
         return 1;
     }
-
-    fprintf(outputFile, "%s\n", fcfsStats.output);
-    fprintf(outputFile, "%.2lf %.2lf\n", fcfsStats.avgTurnaroundTime, fcfsStats.avgResponseTime);
     fclose(inputFile);
     fclose(outputFile);
 
     free(processes);
-    free(processesArray);
 
     printf("Process data and process array written to %s\n", outputFileName);
 
